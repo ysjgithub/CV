e0d0fa194a395c2e3d251e217c57fe4562c8c6e9
杨邵军 <ccnuysj@qq.com>
2020-03-24 20:47:37 +0800

    ucf101-dataset-pytorch

--- experiment/dataset.py
+++ experiment/dataset.py
@@ -1,10 +1,12 @@
-import  torch.utils.data
+import torch.utils.data
 import torch
 import numpy as np
 import os
 import os.path
-import  cv2 as cv
+import cv2 as cv
 import re
+from torchvision import transforms
+
 
 def readfile():
     r = []
@@ -12,44 +14,46 @@ def readfile():
         i = 0
         s = f.readline()
         while s:
-            r.append(re.split(r'[" ","\n"]',s)[:-1])
+            r.append(re.split(r"[' ','\n']", s)[:-1])
             s = f.readline()
-            i+=1
+            i += 1
         f.close()
         return r
+import random
 
-def clipframe(path):
-    cap = cv.VideoCapture(path)
+def clipframe(path,transform):
+    cap = cv.VideoCapture("../vedio/UCF101/{}".format(path))
     imgs = []
     ret, frame = cap.read()
     cur_frame = 0
     while ret:
         if cur_frame % 5 == 0:
+            if transform:
+                frame = transform(frame)
             imgs.append(frame)
         cur_frame += 1
         ret, frame = cap.read()
     cap.release()
+    # print(len(imgs))
+    imgs = random.sample(imgs,7)
+    # print(s)
     return np.array(imgs)
 
-r  = readfile()
-img = clipframe('../vedio/UCF101/v_ApplyEyeMakeup_g01_c01.avi')
-print(torch.from_numpy(img).float().shape)
 
+class MyTrainData(torch.utils.data.Dataset):
 
-class MyTrainData(torch.utils.data.dataset):
-
-    def __init__(self,root,transform=None,train=True):
-        self.root = root
-        self.train = train
+    def __init__(self,transform=None):
         self.filelist = readfile()
+        self.transform = transform
+        # print(self.filelist)
 
     def __getitem__(self, idx):
-
-        imgs = clipframe(self.dirs[idx])
+        # print(self.filelist[idx])
+        imgs = clipframe(self.filelist[idx][0],self.transform)
+        # print(imgs.shape)
         imgs = torch.from_numpy(imgs).float()
-        gt = torch.from_numpy(self.dirs[idx].float())
-        return imgs,gt
+        gt = torch.from_numpy(np.array([int(self.filelist[idx][1])])).float()
+        return imgs, gt
 
     def __len__(self):
-
         return len(self.filelist)
