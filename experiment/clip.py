cb16bb8ed4dd5eed7da9418926d5374298ac5fd1
杨邵军 <ccnuysj@qq.com>
2020-04-01 23:20:37 +0800

    ucf101-dataset-pytorch

---
+++ experiment/clip.py
@@ -0,0 +1,55 @@
+import torch
+import numpy as np
+from torch.utils.data import dataloader, dataset
+import cv2 as cv
+import torchvision.transforms as transforms
+import re
+import random
+import os
+
+def readfile(file_list):
+    r = []
+    for  path in file_list:
+        with open(path) as f:
+            s = f.readline()
+            while s:
+                r.append(re.split(r"[' ','\n']", s)[:-1])
+                s = f.readline()
+            f.close()
+    return r
+
+def clipframe(path,transform):
+    pathv = "../vedio/UCF101/{}".format(path)
+    pathp = "../vedio/UCF101p/{}".format(path.split('.')[-2])
+    cap = cv.VideoCapture(pathv)
+    ret, frame = cap.read()
+    cur_frame = 0
+    if not os.path.exists(pathp):
+        os.makedirs(pathp)
+    while ret:
+        # print(type(frame))
+        if transform:
+            frame = transform(frame)
+        # print(type(frame))
+
+        cv.imwrite("{}/{}.jpg".format(pathp,cur_frame),np.array(frame))
+        cur_frame += 1
+        ret, frame = cap.read()
+    cap.release()
+
+transform = transforms.Compose([
+    transforms.ToPILImage(),
+        transforms.RandomCrop((240,240)),
+])
+
+train_file = ['./file/trainlist01.txt','./file/trainlist02.txt','./file/trainlist03.txt']
+
+filelist = readfile(train_file)
+print(len(filelist))
+# for i in range(len(filelist)):
+#     clipframe(filelist[i][0],transform)
+#     print(i)
+#
+# test_file = ['./file/testlist01.txt','./file/testlist02.txt','./file/testlist03.txt']
+# Training dataset
+
--- experiment/dataset.py
+++ experiment/dataset.py
@@ -10,7 +10,7 @@ from torchvision import transforms
 
 def readfile():
     r = []
-    with open('../ucfTrainTestlist/trainlist01.txt') as f:
+    with open('../file/trainlist01.txt') as f:
         i = 0
         s = f.readline()
         while s:
@@ -29,21 +29,20 @@ def clipframe(path,transform):
     while ret:
         if cur_frame % 5 == 0:
             if transform:
-                frame = transform(frame)
+                frame = transform(frame).numpy()
             imgs.append(frame)
         cur_frame += 1
         ret, frame = cap.read()
     cap.release()
-    # print(len(imgs))
     imgs = random.sample(imgs,7)
-    # print(s)
-    return np.array(imgs)
+    return np.array(imgs).reshape(3,7,240,240)
 
 
 class MyTrainData(torch.utils.data.Dataset):
 
     def __init__(self,transform=None):
-        self.filelist = readfile()
+
+        self.filelist = readfile()[0:10]
         self.transform = transform
         # print(self.filelist)
 
@@ -57,3 +56,30 @@ class MyTrainData(torch.utils.data.Dataset):
 
     def __len__(self):
         return len(self.filelist)
+
+transform = transforms.Compose([
+    transforms.ToPILImage(),
+        transforms.RandomCrop(240),
+        transforms.ToTensor(),
+])
+train_data = torch.utils.data.DataLoader(MyTrainData(transform),shuffle=True,num_workers=4,batch_size=10)
+for x,y in train_data:
+    print(x.shape,y.shape)
+from torch import nn
+def panel(input,output):
+    return nn.Sequential(
+        nn.Conv3d(input, output, 3, 1, 1),
+        nn.BatchNorm3d(64),
+        nn.ReLU(inplace=True),
+        nn.MaxPool3d((1, 2, 2), stride=(1, 2, 2)),
+    )
+
+from torchsummary import summary
+model = nn.Sequential(
+    panel(3,64),
+    panel(64,64),
+    # panel(64,64)
+)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model.to(device)
+summary(model,(3,7,240,240))
